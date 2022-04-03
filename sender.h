e3e6280e2a3515cffc44cc9eccdef19f9fc771db
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-03 20:13:56 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -1,27 +1,29 @@
 #ifndef SENDER_H__
 #define SENDER_H__
 
-class Temperature
-{
-  public:
-    float maximum_temperature;
-    float minimum_temperature;
-    float data[]={};
-};
-
-class SOC
-{
-  public:
-    float maximum_soc;
-    float minimum_soc;
-    float data[]={};
-};
-
 class Sender
 {
   public:
     int number_of_values;
     void generateData(Temperature temperature,SOC soc,Sender sender,void (*)(Temperature temperature,SOC soc,Sender sender));
+};
+
+class Temperature
+{
+  public:
+    Sender sender;
+    float maximum_temperature;
+    float minimum_temperature;
+    float data[sender.number_of_values];
+};
+
+class SOC
+{
+  public:
+    Sender sender;
+    float maximum_soc;
+    float minimum_soc;
+    float data[sender.number_of_values];
 };
 
 #endif
