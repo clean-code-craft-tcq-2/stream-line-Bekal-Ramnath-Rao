9a1176abb827ae00bd0967b9af37f51f47217793
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-03 20:40:10 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -19,7 +19,7 @@ TEST_CASE("Prepare Data") {
   sender.number_of_values = 5;
   int temperature_data[sender.number_of_values] = {21,31,41,51,61};
   int soc_data[sender.number_of_values] = {21,25,30,35,39};
-  void *funp_generateData(Temperature,SOC,Sender) = generateManualData;
+  void (*funp_generateData)(Temperature,SOC,Sender) = generateManualData;
   sender.generateData(temperature,soc,sender,funp_generateData);
   for(int i=0; i<sender.number_of_values; i++)
   {
