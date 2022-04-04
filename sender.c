2b950863d582c5ffc90bf3668d87308cccc9c8cd
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-04 07:03:10 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -1,6 +1,6 @@
 #include "sender.h"
 
-void Sender::generateData(Temperature temperature,SOC soc,Sender sender,void (*funp_generateData)(Temperature temperature,SOC soc,Sender sender))
+void Sender::generateData(Temperature temperature,SOC soc,Sender sender,void (*funp_generateData)(Temperature,SOC,Sender))
 {
     funp_generateData(temperature,soc,sender);
 }
