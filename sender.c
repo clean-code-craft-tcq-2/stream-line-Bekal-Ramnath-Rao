64a1623e006931d11c7c15e0b0825d1803333ad8
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-04 08:38:49 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -4,16 +4,5 @@ using namespace std;
 
 void Sender::generateData(Temperature &temperature,SOC &soc,Sender &sender,void (*funp_generateData)(Temperature&,SOC&,Sender&))
 {
-  float manualtemperature_data[sender.number_of_values] = {21,31,41,51,61};
-  float manualsoc_data[sender.number_of_values] = {21,25,30,35,39};
-  for(int i=0;i<sender.number_of_values;i++)
-  {
-    temperature.data[i] = manualtemperature_data[i];
-    soc.data[i] = manualsoc_data[i];
-  }
-  for(int i=0;i<sender.number_of_values;i++)
-  {
-    cout<<temperature.data[i]<<endl;
-    cout<<soc.data[i]<<endl;
-  }
+  funp_generateData(&temperature,&soc,&sender);
 }
