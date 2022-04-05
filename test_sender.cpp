95db74d6b3e1e077df4d3a9b2496c540d8ea43ee
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-05 07:18:52 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -28,7 +28,7 @@ TEST_CASE("Prepare Data") {
   sender.number_of_values = 5;
   float Expectedtemperature_data[sender.number_of_values] = {21,31,41,51,61};//assuming these are generated values
   float Expectedsoc_data[sender.number_of_values] = {21,25,30,35,39};
-  void (*funp_generateData)(Temperature&,SOC&,Sender&) = generateManualData;
+  void (*funp_generateData)(float,float) = generateManualData;
   void (*funp_formatData)(Temperature&,SOC&,Sender&) = formatData;
   void (*funp_printonConsole)(Sender&) = printonConsole;
   sender.generateData(temperature,soc,sender,funp_generateData,funp_formatData,funp_printonConsole);
