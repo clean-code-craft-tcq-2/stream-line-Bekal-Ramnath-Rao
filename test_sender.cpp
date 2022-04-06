43eef1ce5d69ca385209ef7f96efbb60a3b289f6
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-06 08:18:33 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -6,10 +6,23 @@
 using namespace std;
 
 int generateData_Called;
-float generateManualData(float parameter_Maximum,float parameter_Minimum)
+int formatData_Called;
+int printonConsole_Called;
+
+float generateData_test(float parameter_Maximum,float parameter_Minimum)
 {
   generateData_Called = 1;
   return 1.0;
+}
+
+void formatData_test(Temperature &temperature,SOC &soc,Sender &sender)
+{
+  formatData_Called = 1;
+}
+
+void printonConsole_test(Temperature &temperature,SOC &soc,Sender &sender)
+{
+  printonConsole_Called = 1;
 }
 
 TEST_CASE("Prepare Data") {
@@ -23,13 +36,18 @@ TEST_CASE("Prepare Data") {
   sender.number_of_values = 5;
   float Expectedtemperature_data[sender.number_of_values] = {21,31,41,51,61};//assuming these are generated values
   float Expectedsoc_data[sender.number_of_values] = {21,25,30,35,39};
-  float (*funp_generateData)(float,float) = generateManualData;
+  float (*funp_generateData)(float,float) = generateRandomData;
   void (*funp_formatData)(Temperature&,SOC&,Sender&) = formatData;
   void (*funp_printonConsole)(Sender&) = printonConsole;
   sender.generateandFormatData(temperature,soc,sender,funp_generateData,funp_formatData,funp_printonConsole);
-  REQUIRE( generateData_Called == 1);
+  REQUIRE( generateData_Called == 0);
   
   sender.number_of_values = 20;
-  funp_generateData = generateRandomData;
+  funp_generateData = generateData_test;
+  funp_formatData = formatData_test;
+  funp_printonConsole = printonConsole_test;
   sender.generateandFormatData(temperature,soc,sender,funp_generateData,funp_formatData,funp_printonConsole);
+  REQUIRE( generateData_Called == 1);
+  REQUIRE( formatData_Called == 1);
+  REQUIRE( printonConsole_Called == 1);
 }
