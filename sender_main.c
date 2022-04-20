441f15a41456ad2b66a9d902e57f7a2347a6f871
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-04-20 10:41:08 -0500

    Added main function to sender

---
+++ sender_main.c
@@ -0,0 +1,20 @@
+
+#include "test/catch.hpp"
+#include "sender.h"
+
+int main(){
+
+    Temperature temperature;
+    SOC soc;
+    Sender sender;
+    temperature.maximum= 80;
+    temperature.minimum= 20;
+    soc.maximum = 40;
+    soc.minimum = 20;
+    sender.number_of_values = 50;
+    float (*funp_generateData)(float,float) = generateRandomData;
+    void (*funp_formatData)(Temperature&,SOC&,Sender&) = formatData;
+    void (*funp_printonConsole)(Sender&) = printonConsole;
+    sender.generateandFormatData(temperature,soc,sender,funp_generateData,funp_formatData,funp_printonConsole);
+
+}
